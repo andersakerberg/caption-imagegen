3ccc0e6745675f015f7c18224927472c7d0ae51c
Mgs <akerberganders@gmail.com>
2024-02-21 11:22:33 +0000

    added it all

---
+++ src/app/components/ImageUpload.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React, { useState } from "react";
+import { CaptionGenerator } from "../services/ImageCaption";
+
+const capGen = new CaptionGenerator();
+
+interface ImageFile {
+  name: string;
+  size: number;
+  type: string;
+}
+
+const ImageUpload: React.FC = () => {
+  const [file, setFile] = useState<ImageFile | null>(null);
+  const [base64Image, setBase64Image] = useState("");
+  const [predictions, setPredictions] = useState([]);
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPredictions([]);
+    //@ts-ignore
+    setFile(event.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      return;
+    }
+    setIsGenerating(true);
+
+    const reader = new FileReader();
+    reader.onload = async () => {
+      //@ts-ignore
+      setBase64Image(reader.result.split(",")[1]); // Skip 'data:image/png;base64,' prefix
+      //@ts-ignore
+      console.log(reader.result.split(",")[1]);
+      //@ts-ignore
+      const responseFromApi = await capGen.generateCaptions(
+        //@ts-ignore
+        reader.result.split(",")[1]
+      );
+      console.log(responseFromApi);
+      if (responseFromApi && responseFromApi.length > 0) {
+        //@ts-ignore
+        setPredictions(responseFromApi);
+      }
+
+      setIsGenerating(false);
+    };
+    //@ts-ignore
+    reader.readAsDataURL(file);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} accept="image/*" />
+      <button onClick={handleUpload}>Upload Image</button>
+      {isGenerating && <div className="loader">Loading...</div>}
+      {base64Image && <p>Base64 image: {base64Image.slice(0, 50)}</p>}
+      {base64Image && <img src={`data:image/jpeg;base64,${base64Image}`} />}
+      {predictions &&
+        predictions.map((text, i) => {
+          console.log(text);
+          // Return the element. Also pass key
+          return <p key={i}>{text}</p>;
+        })}
+    </div>
+  );
+};
+
+export default ImageUpload;
---
+++ src/app/services/ImageCaption.ts
@@ -0,0 +1,59 @@
+interface ServiceAccount {
+    client_email: string;
+    private_key: string;
+}
+
+interface Image {
+    content: string; // Base64 encoded image
+}
+
+interface PredictRequest {
+    instances: {
+        image: Image;
+    }[];
+}
+
+interface PredictResponse {
+    predictions: {
+        text: string;
+    }[];
+}
+
+export class CaptionGenerator {
+    constructor() {
+
+    }
+
+    async generateCaptions(base64Image: string): Promise<string[]> {
+        try {
+            const accessToken = this.getAccessToken();
+            const url = `https://us-central1-aiplatform.googleapis.com/v1/projects/rtw-armory/locations/us-central1/publishers/google/models/imagetext:predict`;
+
+
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    instances: [{
+                        image: {
+                            bytesBase64Encoded: base64Image,
+                        },
+                    }],
+                }),
+            });
+
+            const data = await response.json();
+            return data.predictions;
+        } catch (error) {
+            console.error('Error generating captions:', error);
+            throw error; // Re-throw for external handling
+        }
+    }
+
+    private getAccessToken(): string {
+        return "ya29.a0AfB_byA8-pp0B6U0wduSJjDpIcjIH7V3aWYA8Vq9cCEWZNzxoeEiVkQmpMuh4WDNkpK4X9HxgLFgKGrW-RmnYooSyYhj0LS5raOre82Q8FUwCUWHNQB2rTJ3L_Pu2zL1lLGL5_xffwSwrPnmuw6Ee5YXN_c8A5oVTQaCgYKAdcSARISFQHGX2MifOe0Im2mqOWTjUT742TKVw0169"
+    }
+}
